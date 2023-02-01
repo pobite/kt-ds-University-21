c32e20c11a10fb7f1385ddaa56c4c7d113e0cae7
User <User@211-02>
2023-02-01 13:41:05 +0900

    초기화

---
+++ todoList/src/come/ktdsuniversity/edu/todo/service/TodoService.java
@@ -0,0 +1,43 @@
+package come.ktdsuniversity.edu.todo.service;
+
+import java.util.List;
+
+import come.ktdsuniversity.edu.todo.dao.TodoDAO;
+import come.ktdsuniversity.edu.todo.vo.TodoVO;
+
+public class TodoService {
+	
+	private TodoDAO todoDAO;
+	
+	public TodoService() {
+		todoDAO = new TodoDAO();		
+	}
+	
+	// 등록
+	public boolean create(String itemName) {
+		return todoDAO.create(itemName) > 0;
+	}
+	// 조회(1건)
+	public TodoVO read(int key) {
+		return todoDAO.read(key);
+	}
+	// 조회 (모든건)
+	public List<TodoVO> readAll() {
+		return todoDAO.readAll();
+	}
+	
+	// 수정
+	public boolean update(int key, boolean isSucess) {
+		return todoDAO.update(key, isSucess) > 0;
+	}
+	
+	// 삭제 (1건)
+	public boolean delete(int key) {
+		return todoDAO.delete(key) > 0;		
+	}
+	
+	// 삭제 (모든건)
+	public int deleteAll() {
+		return todoDAO.ddeleteAll();
+	}
+}
---
+++ todoList/src/come/ktdsuniversity/edu/todo/vo/TodoVO.java
@@ -0,0 +1,29 @@
+package come.ktdsuniversity.edu.todo.vo;
+
+public class TodoVO {
+	
+	private String itemName;
+	private boolean isSuccess;
+	
+	public TodoVO(String itemName) {
+		setItemName(itemName);
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public boolean isSuccess() {
+		return isSuccess;
+	}
+
+	public void setSuccess(boolean isSuccess) {
+		this.isSuccess = isSuccess;
+	}
+	
+	
+}

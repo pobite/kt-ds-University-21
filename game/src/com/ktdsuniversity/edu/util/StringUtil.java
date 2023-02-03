1eb90bc65c3126985c66fcc9d5c35291d9476525
pobite <ehdgkr5986@naver.com>
2023-02-03 11:02:18 +0900

    초기화

---
+++ game/src/com/ktdsuniversity/edu/util/StringUtil.java
@@ -0,0 +1,16 @@
+package com.ktdsuniversity.edu.util;
+
+public abstract class StringUtil {
+
+	private StringUtil() {}
+	
+	public static String startWith(String string) {
+		
+		
+		return string
+		
+	}
+	
+	
+	
+}

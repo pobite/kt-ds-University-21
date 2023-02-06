33abc79367e1214b3e0ae127e3b916a4a444a307
pobite <ehdgkr5986@naver.com>
2023-02-06 15:07:10 +0900

    UPDATE

--- stream/src/com/ktdsuniversity/edu/stream/BaseballStream1.java
+++ stream/src/com/ktdsuniversity/edu/stream/BaseballStream1.java
@@ -43,6 +43,48 @@ public class BaseballStream1 {
 	
 	public static void main(String[] args) {
 		List<AllStarFullVO> list = readAllStarFull();
-		System.out.println(list.size());
+		
+		list.stream()
+			.filter( (vo) -> vo.getYear().equals("1933") )
+			.forEach( (vo) -> {
+				System.out.println(vo.getPlayerID());
+				System.out.println(vo.getYear());
+			} );
+		
+		List<AllStarFullVO> playIn1933 = list.stream()
+											.filter( (vo) -> vo.getYear().equals("1933"))
+											.collect(Collectors.toList());
+		
+		AllStarFullVO vo  = list.stream()
+								.filter( (vo1) -> vo1.getYear().equals("1933") )
+								.findFirst()	// 최종 함수
+//								.get();			// optional의 메소드
+								.orElse(new AllStarFullVO());
+								// 데이터 존재하면 첫번째꺼 돌려주고, 없으면 괄호 안의 것 리턴
+								// 많이 쓰임.
+		
+		System.out.println(vo.getPlayerID());
+		
+		
+		
+		/**
+		 * 실습
+		 * 연도에 상관없이 플레이어 ID가 f로 시작하는 데이터 모두 출력
+		 */
+		
+		// f로 시작하는 PlayerID를 리스트로
+		List<AllStarFullVO> playStartWithF = list.stream()
+											     .filter( (vo2) -> vo2.getPlayerID().toUpperCase().startsWith(("f")))
+											     .collect(Collectors.toList());
+		
+	
+		System.out.println("-----------------------------------------------------------------");
+		
+		// f로 시작하는 PlayerID 출력
+		list.stream()
+		    .filter( (allStarVO) -> allStarVO.getPlayerID().toUpperCase().startsWith(("F")))
+		    .forEach( (allStarVO) -> {
+		    	System.out.println(allStarVO.getPlayerID());
+	     }) ;
 	}
 }

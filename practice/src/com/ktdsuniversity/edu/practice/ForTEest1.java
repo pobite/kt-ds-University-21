e87496b34f07f005219e9f8d3308466092bfad6e
pobite <ehdgkr5986@naver.com>
2023-02-09 16:35:23 +0900

    add

--- practice/src/com/ktdsuniversity/edu/practice/ForTEest1.java
+++ practice/src/com/ktdsuniversity/edu/practice/ForTEest1.java
@@ -48,65 +48,7 @@ public class ForTEest1 {
 		
 		System.out.println("List를 for문으로 구한 평균: " + avg);
 		
-		
-		/**
-		 * 0 ~ 99까지 출력
-		 */
-		
-		for (int i = 0; i < 100; i++) {
-			System.out.println(i);
-		}
-		
-		/**
-		 * 0 ~ 99까지 출력 && 5의 배수 출력
-		 */
-		
-		for (int i = 0; i < 100; i++) {
-			
-			if (i % 5 == 0) {
-				System.out.println(i);
-			}
-		}
-		
-		/**
-		 * 0 ~ 99까지 출력 && 5의 배수 출력 && 0 출력 x
-		 */
-		
-		for (int i = 0; i < 100; i++) {
-			if (i == 0) {
-				continue;
-			}
-			if (i % 5 == 0) {
-				System.out.println(i);
-			}
-		}
-		
-		/**
-		 * 0 ~99까지
-		 * 짝수면 짝수입니다 출력
-		 * 홀수면 홀수입니다 출력
-		 */
-		
-		for (int i = 0; i < 100; i++) {
-			if (i % 2 == 0) {
-				System.out.println("짝수입니다.");
-			}
-			else {
-				System.out.println("홀수입니다.");
-			}
-		}
-		
-		/**
-		 * 정수형 배열, 인덱스의 개수는 50개
-		 */
-		
-		int[] arr = new int[50];
-		
-		
-		for (int i = 0; i < 50; i++) {
-			
-			
-		}
+	
 	}
 	
 }
--- practice/src/com/ktdsuniversity/edu/practice/ForTest2.java
+++ practice/src/com/ktdsuniversity/edu/practice/ForTest2.java
@@ -63,5 +63,10 @@ public class ForTest2 {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = random.nextInt(50) + 1;
 		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
 	}
 }
